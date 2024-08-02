aa5dbd5c0a42d3f8644ab4f21c26af090f36f03c
Ankit Rajuji Waware <ankitwaware15@gmail.com>
2024-08-02 23:41:32 +0530

    fixed server action error

--- apps/web/components/transfer/AddMoney.tsx
+++ apps/web/components/transfer/AddMoney.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Card from "@repo/ui/Card";
 
-import addMoneyTouser from "../../lib/actions/actions";
-import { useFormState, useFormStatus } from "react-dom";
+import { addMoneySchema, addMoneyInput } from "../../schema/addMoneySchema";
+import { Form, useForm, FormSubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createOnrampTransaction } from "../../lib/actions/createOnrampTransaction";
 
 const supported_banks = [
   { name: "Hdfc Bank", redirectUrl: "https://netbanking.hdfcbank.com" },
@@ -10,15 +12,62 @@ const supported_banks = [
   { name: "Kotak Bank", redirectUrl: "https://www.kotakbank.com/" },
 ];
 
-const initialState = { message: "" };
-
 export default function AddMoney({ className }: { className?: string }) {
-  const [state, fromAction] = useFormState(addMoneyTouser, initialState);
-  const { pending } = useFormStatus();
+  const {
+    control,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<addMoneyInput>({
+    resolver: zodResolver(addMoneySchema),
+    defaultValues: {
+      bank: supported_banks[0]?.name,
+    },
+    progressive: true,
+  });
+
+  const onAddMoneyHandler: FormSubmitHandler<addMoneyInput> = async (
+    payload,
+  ) => {
+    const { data } = payload;
+    console.log(data)
+
+    const { bank, amount } = data;
+    // redirect url of selected Bank
+    // const redirectUrl = supported_banks.find(
+    //   (supp_bank) => supp_bank.name === bank,
+    // )?.redirectUrl;
+
+    try {
+      // transaction unique token
+      const token = (Math.random() * 1000 + 1).toString();
+
+      const NewTxn = await createOnrampTransaction(bank, amount, token);
+
+      // window.location.href = redirectUrl || "";
+
+      // fake bank api to handle add amount
+      const bankUrl = `http://localhost:8080/hdfcwebhook`;
+      fetch(bankUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+          userId: NewTxn.tnx?.userId?.toString(),
+          amount: amount,
+        }),
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <form
-      action={fromAction}
+    <Form
+      control={control}
+      onSubmit={onAddMoneyHandler}
       className={`${className}`}
     >
       <Card title="Add Moeny" className="h-72 justify-between">
@@ -26,19 +75,20 @@ export default function AddMoney({ className }: { className?: string }) {
           <label htmlFor="amount">Amount</label>
           <input
             id="amount"
+            {...register("amount")}
             type="text"
-            name="amount"
             placeholder="Amount"
             autoComplete="off"
             className="rounded-md border border-slate-300 p-2"
           />
+          {<p className="text-sm">{errors.amount?.message}</p>}
         </div>
 
         <div className="flex flex-col gap-y-2">
           <label htmlFor="bank">Bank</label>
           <select
             id="bank"
-            name="bank"
+            {...register("bank")}
             className="rounded-md border border-slate-300 p-2 capitalize"
           >
             {supported_banks.map((bank, index) => {
@@ -51,18 +101,14 @@ export default function AddMoney({ className }: { className?: string }) {
           </select>
         </div>
 
-        <p aria-live="polite" role="status">
-          {state.message && state.message}
-        </p>
-
         <button
           type="submit"
-          aria-disabled={pending}
           className="self-center rounded-md bg-gray-950 p-2 px-3 text-white"
+          disabled={isSubmitting}
         >
-          {pending ? "Adding..." : "Add Money"}
+          {isSubmitting ? "Adding..." : "Add Money"}
         </button>
       </Card>
-    </form>
+    </Form>
   );
 }
